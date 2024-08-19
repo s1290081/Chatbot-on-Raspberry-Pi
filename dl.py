b180231ea022393217d2ae43d05c106201e02344
s1290081_YutoHyakumoto <100057614+s1290081@users.noreply.github.com>
2024-08-19 20:34:21 +0900

    Delete dl.py

--- dl.py
+++
@@ -1,9 +0,0 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
-
-model_name = "gpt2"
-model = AutoModelForCausalLM.from_pretrained(model_name)
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-
-# Save models and tokenizers
-model.save_pretrained("./gpt2_model")
-tokenizer.save_pretrained("./gpt2_model")
